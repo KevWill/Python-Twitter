24301d055e7709a82b96ed04963fa3dfd4d7ab17
KevWill <kevin.willemsen@gmail.com>
2017-01-08 20:48:38 +0100

    removed twitter config

--- twitter_config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'LuehN69lyUPimz17P6PubMF1m'
-consumer_secret = 'KdWeygwWQrYogq5htbHAJHstCuI2iQredhzwGW9qd42LXxInKg'
-access_token = '502751658-hlkTCGwyLHAAhGfVnJNaF9LvTXOdmumBpqb3M8BZ'
-access_secret = 'NHhNcnoloVvv5GyQN0uOOBvmhzd2DCLeT8Dc2kiAIOFN9'
